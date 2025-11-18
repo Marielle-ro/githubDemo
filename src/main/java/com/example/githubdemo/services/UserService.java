fbce94e3ea9af1ed9e534514fac80e790095f177
roxane-hub <marielleroxane037@gmail.com>
2025-11-18 10:43:15 +0200

    creates the business logic for creating new user

--- src/main/java/com/example/githubdemo/services/UserService.java
+++ src/main/java/com/example/githubdemo/services/UserService.java
@@ -1,4 +1,22 @@
 package com.example.githubdemo.services;
 
+import com.example.githubdemo.dtos.UserDto;
+import com.example.githubdemo.entities.Users;
+import com.example.githubdemo.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    public final UserRepository repo;
+
+    public UserService(UserRepository repo) {
+        this.repo = repo;
+    }
+    public Users addUser(UserDto userDto) {
+        Users user = new Users();
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setEmail(userDto.getEmail());
+        return repo.save(user);
+    }
 }
