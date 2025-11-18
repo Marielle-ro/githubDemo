788093d0f832830678eb88dd0cf117c82db0512f
roxane-hub <marielleroxane037@gmail.com>
2025-11-18 09:59:02 +0200

    created a user entitiy

---
+++ src/main/java/com/example/githubdemo/entities/Users.java
@@ -0,0 +1,20 @@
+package com.example.githubdemo.entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+}
